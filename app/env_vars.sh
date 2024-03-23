c6ef781214e752f7f792692f670695ddac55d3c9
ksmin23 <sonagy23@gmail.com>
2024-03-23 16:14:08 +0900

    refactor codes

--- app/env_vars.sh
+++ app/env_vars.sh
@@ -1,3 +1,3 @@
-export AWS_REGION="<YOUR-AWS-REGION>"
+export AWS_REGION="us-east-1"
 export KENDRA_INDEX_ID="<YOUR-KENDRA-INDEX-ID>"
-export BEDROCK_MODEL_ID="<BEDROCK-MODEL-ID>"
+export BEDROCK_MODEL_ID="anthropic.claude-v2:0:18k"
